5a20f0489e8ef30ef0152253baf7e4fa6e39bdf8
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-10-24 07:46:14 -0500

    chore(home): add link to products

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -3,6 +3,7 @@ import { Slider, Button, Wrapper } from "../../Common"
 import { CategoryList } from "../../Category/CategoryList"
 import { FeaturedProducts } from "../../Products/FeaturedProducts";
 import { ELEMENT_SIZE } from "../../../utils/constants";
+import { Link } from "react-router-dom";
 import featuredBanners from '../../../mocks/en-us/featured-banners.json';
 import productCategories from '../../../mocks/en-us/product-categories.json';
 import featuredProducts from '../../../mocks/en-us/featured-products.json';
@@ -18,7 +19,9 @@ function Home(){
             <CategoryList categories={categoriesData} />
             <FeaturedProducts products={productsData} />
             <Wrapper className="home__button" flex justify="center" margin="30px">
-                <Button size={ELEMENT_SIZE.LG}>View all products</Button>
+                <Link to="/products">
+                    <Button size={ELEMENT_SIZE.LG}>View all products</Button>
+                </Link>
             </Wrapper>
         </Fragment>
     )
