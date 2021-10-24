2d0e751835f7ebc2c4d0f0d806d071e824b8b967
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-10-24 06:01:00 -0500

    chore(common): update wrapper component

--- src/components/Common/Wrapper/index.js
+++ src/components/Common/Wrapper/index.js
@@ -2,16 +2,22 @@ import React from "react";
 import PropTypes from 'prop-types';
 import { StyledWrapper } from "./styled"
 
-Wrapper.propTypes = {
-    className: PropTypes.string,
-};
-
 function Wrapper(props){
 
     return (
-        <StyledWrapper className={props.className ? props.className : ''} >
+        <StyledWrapper settings={props} className={props.className ? props.className : ''} >
             {props.children}
         </StyledWrapper>
     )
 }
+
+Wrapper.propTypes = {
+    className: PropTypes.string,
+    justify: PropTypes.string,
+    margin: PropTypes.string,
+    padding: PropTypes.string,    
+    flex: PropTypes.bool,
+    flexColumn: PropTypes.bool,
+};
+
 export { Wrapper }
