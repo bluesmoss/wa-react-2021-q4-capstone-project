0b838eb7681a0109cb09200369cd359c7f47a6b8
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-10-29 01:05:39 -0500

    chore(pages): update folder structure

--- src/components/Layout/Main/index.js
+++ src/components/Layout/Main/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { Home, Products } from "../../../Pages"
+import { Home, Products } from "../../../pages"
 import { StyledMain } from './styled'
 
 function Main(){
