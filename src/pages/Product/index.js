ce69441a1e2811f2277102ca9754e8a9fd126b97
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-11-06 17:17:26 -0600

    chore(product): add loading component

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -4,69 +4,85 @@ import { useParams} from "react-router-dom";
 import { useProductDetails } from "../../utils/hooks/useProductDetails";
 import  cart from "../../assets/cart.png"
 import { StyledProduct } from "./styled"
+import { TEXT_COLOR_TYPES } from "../../utils/constants";
+import { Fragment } from "react/cjs/react.production.min";
 
 
 function Product(){
     const { productId } = useParams();
-    const { data : product } = useProductDetails(productId);
-     
+    const { data : product, isLoading} = useProductDetails(productId);
     return (
-        <StyledProduct>
-            <Wrapper className="product_gallery">
-                <Gallery slides={product.images} />                
-            </Wrapper>
-            <Wrapper className="product_info">
-                <Wrapper>
-                    <label>Name:</label> {product.name}
-                </Wrapper>
-                <Wrapper>
-                    <label>Price:</label> {product.price}
-                </Wrapper>
-                <Wrapper>
-                    <label>SKU:</label> {product.sku}
-                </Wrapper>
-                <Wrapper>
-                    <label>Category:</label> {product.category?.slug}
-                </Wrapper>
-                <Wrapper>
-                    <label>Tags: </label> 
-                    {
-                    product.tags?.length
-                        ? 
-                        
-                        product.tags.map(tag => (
-                            <li key={tag}>{tag}</li>
-                            ))
-                        : <TextColored>No tags found</TextColored>
-                    }                
-                </Wrapper>
-                <Wrapper>
-                    <label>Description:</label> <p>{product.short_description}</p>    
-                </Wrapper>
-                <Wrapper>
-                    <label>Items to be added:</label>
-                    <input type="number"/>
-                    <button className="product__add-cart">
-                                <img  className="product__cart"  src={cart} alt="cart"/>
-                    </button>                  
-                </Wrapper>
-                <Wrapper>
-                    <label>Spec:</label>
-                    <ul>
-                    {
-                    product.specs?.length
-                        ? 
-                        
-                        product.specs.map( (spec, index) => (
-                            <li key={`spec${index}`}>{spec.spec_name} :  {spec.spec_value}</li>
-                            ))
-                        : <li><TextColored>No spec found</TextColored></li>
-                    }   
-                    </ul>
 
-                </Wrapper>                    
-            </Wrapper>                                       
-        </StyledProduct>
+        <Fragment>
+        {isLoading && 
+            <Wrapper flex padding="100px">
+                <TextColored color={TEXT_COLOR_TYPES.SECONDARY}>Loading....</TextColored>
+            </Wrapper>
+        }
+
+        {!isLoading &&
+            <StyledProduct>
+                <Wrapper className="product_gallery">
+                    <Gallery slides={product.images} />                
+                </Wrapper>
+                <Wrapper className="product_info">
+                    <Wrapper>
+                        <label>Name:</label> {product.name}
+                    </Wrapper>
+                    <Wrapper>
+                        <label>Price:</label> {product.price}
+                    </Wrapper>
+                    <Wrapper>
+                        <label>SKU:</label> {product.sku}
+                    </Wrapper>
+                    <Wrapper>
+                        <label>Category:</label> {product.category?.slug}
+                    </Wrapper>
+                    <Wrapper>
+                        <label>Tags: </label> 
+                        {
+                        product.tags?.length
+                            ? 
+                            
+                            product.tags.map(tag => (
+                                <li key={tag}>{tag}</li>
+                                ))
+                            : <TextColored color={TEXT_COLOR_TYPES.SECONDARY}>No tags found</TextColored>
+                        }                
+                    </Wrapper>
+                    <Wrapper>
+                        <label>Description:</label> <p>{product.short_description}</p>    
+                    </Wrapper>
+                    <Wrapper>
+                        <label>Items to be added:</label>
+                        <input type="number"/>
+                        <button className="product__add-cart">
+                                    <img  className="product__cart"  src={cart} alt="cart"/>
+                        </button>                  
+                    </Wrapper>
+                    <Wrapper>
+                        <label>Spec:</label>
+                        <ul>
+                        {
+                        product.specs?.length
+                            ? 
+                            
+                            product.specs.map( (spec, index) => (
+                                <li key={`spec${index}`}>{spec.spec_name} :  {spec.spec_value}</li>
+                                ))
+                            : <li><TextColored key="no-items" color={TEXT_COLOR_TYPES.SECONDARY}>No spec found</TextColored></li>
+                        }   
+                        </ul>
+
+                    </Wrapper>                    
+                </Wrapper>                                       
+            </StyledProduct>            
+
+        }
+        
+        </Fragment>
+ 
+
     )
 }
 export { Product }
